5d32f1a342806b983f8d9778ba1f8663749f5c16
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-06-23 16:17:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ USERNAME = environ.get('USERNAME', "") # ADMIN USERNAME
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 
 # ForceSub Channel & Log Channels
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002838081440'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002395713472'))
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002838081440'))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002395713472'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002395713472')) 
